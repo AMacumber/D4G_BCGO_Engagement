a8bc75586ce2aea941eb7f67dd66bd637aa2b4e8
AMacumber_Desktop <andrew.l.macumber@gmail.com>
2020-02-28 08:19:18 -0500

    Continued data viz of important features; distance and visits done; working on age

--- Step_08_DataShow.R
+++ Step_08_DataShow.R
@@ -25,39 +25,81 @@ df_000 <- read.csv(file = "D4G_BGCO_Engage_Labeled_ForEDA_v003.csv", stringsAsFa
 #
 #############################################################################################################
 #
+## Set Theme
+
+# Create a new theme
+theme_blackborder_empty <- function (base_size = 11, base_family = "") {
+  theme_bw() %+replace% 
+    theme(
+      panel.grid.major  = element_line(color = "white"),
+      panel.grid.minor  = element_line(color = "white"),
+      panel.background = element_rect(fill = "white"),
+      panel.border = element_rect(color = "black", fill = NA),
+      axis.line = element_line(color = "black"),
+      axis.ticks = element_line(color = "black"),
+      axis.text = element_text(color = "black")
+    )
+}
+##
+#
+#############################################################################################################
+#
 ## Histogram - Count of Engaged Members by Distance
 
 # Keep only engaged members
-df_engaged <- df_000 %>%
+df_distance <- df_000 %>%
   
   filter(label == "engaged") %>%
   filter(club_km < 20)
 
-ggplot(df_engaged, aes(club_km)) +
+ggplot(df_distance, aes(club_km)) +
   geom_histogram( binwidth=1.25, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
-  annotate("text", x=5, y=133, label= "Bars are roughly every 1 km") + 
-  annotate("text", x=5, y=133, label= "Bars are roughly every 1 km") + 
-  labs(title = "How far Engaged Members travel", x="Distance to Clubhouse(km)", y="Number of Engaged Members", fill = "Engaged")
+  annotate("text", x=5, y=133, label= "Bar widths are roughly every 1 km") + 
+  labs(title = "How far Engaged Members travel", x="Distance to Clubhouse(km)", y="Number of Engaged Members", fill = "Engaged") +
+  theme_blackborder_empty()
 ##
 #
 #############################################################################################################
 #
 ## Stacked Barplot - Proportion of Static vs Engaged by visits
-# need to bin visits
-# show proportion of static/engaged by visit bin
+
+# Convert number of visits to a categorical variable
+hist(df_000$checkin_total, breaks = 8)
+
+df_visits <- df_000 %>%
+  
+  mutate(visit_bins = cut(checkin_total, breaks = 8, labels = c("0", "5", "10", "15", "20", "25", "30", "35"))) %>%
+  
+  group_by(visit_bins, label) %>%
+  summarise(Count_Members =n())
 
 # Plot the basic frame of the stacked bar chart.
-ggplot(df_000, aes(x = PTA_Names, y = Count_Programs, fill = Data_QA)) + 
+ggplot(df_visits, aes(x = visit_bins, y = Count_Members, fill = label)) + 
   theme_blackborder_empty() +
   geom_bar(position = "fill", stat = "identity") +
   scale_y_continuous(labels = scales::percent_format()) +
-  scale_fill_manual(values=c("#098553", "#4ca64c", "#99cc99", "#cccccc")) +
-  guides(fill = guide_legend(reverse = TRUE)) +
-  labs(title = paste("Program Data Quality by Region for", year, sep = " "), x="", y="", fill = "") +
-  theme(legend.position="top") +
-  coord_flip()
+  #scale_fill_manual(values=c("#098553", "#4ca64c", "#99cc99", "#cccccc")) +
+  #guides(fill = guide_legend(reverse = TRUE)) +
+  labs(title = "Year 1 Clubhouse Visits", x="Number of Visits", y="", fill = "Year Two Status:") +
+  theme(legend.position="top")
+  #coord_flip()
 ##
 #
 #############################################################################################################
 #
-# Histogram - Count of engaged vs Age
+# ECDF - Count of engaged vs Age
+# add lines to show that engaged members tend to be younger
+
+ggplot(df_000, aes(x= Y1_Age, fill=label)) +
+  geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity', bins = 20) +
+  scale_fill_manual(values=c("#69b3a2", "#404080")) +
+  labs(fill="")
+
+df_age <- df_000 %>% filter(label == 'engaged')
+
+ggplot(df_000, aes(Y1_Age, colour = label)) + 
+  theme_blackborder_empty() +
+  stat_ecdf(geom = "line", size = 2) +
+  guides(fill=guide_legend(title="Year Two Status:")) +
+  labs(title = "Member Age Distributions", x="Age of Member During Year One", y="") +
+  theme(legend.position="top")
